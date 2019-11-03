a375edbe4516144efb61211b77a09f6986471add
bbkgl <zoujk2333@qq.com>
2019-11-03 12:45:12 +0800

    update hive data-analysis

--- hadoop_add_data.sh
+++ hadoop_add_data.sh
@@ -1,4 +1,6 @@
-hadoop fs -mkdir /sogou
-hadoop fs -mkdir /sogou/data
+start-dfs.sh
+start-yarn.sh
+hadoop fs -mkdir -p /sogou
+hadoop fs -mkdir -p /sogou/data
 hadoop fs -put data/sogou.500w.utf8 /sogou/data
-hadoop fs -put data/sogou.500w.utf8.ext /sogou/data
+hadoop fs -put data/sogou.500w.utf8.flt /sogou_ext/data
--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-infile=data/sogou.500w.utf8
+infile1=data/sogou.500w.utf8
 # infile=$1
-outfile=data/sogou.500w.utf8.ext
+infile2=data/sogou.500w.utf8.ext
 # outfile=$2
-awk -F '\t' '{print $0"\t"substr($1,0,5)"year\t"substr($1,5,2)"month\t"substr($1,7,2)"day\t"substr($1,8,2)"hour"}' $infile > $outfile
+awk -F '\t' '{print $0"\t"substr($1,0,5)"year\t"substr($1,5,2)"month\t"substr($1,7,2)"day\t"substr($1,8,2)"hour"}' $infile1 > $infile2
+outfile=data/sogou.500w.utf8.flt
+awk -F "\t" '{if($2 != "" && $3 != "" && $2 != " " && $3 != " ") print $0}' $infile2 > $outfile
 
